c356d9c977f3ecc9a3769d42c65e554c1a6601d3
Carlos Tarmeno <carlos-100-800@hotmail.com>
2023-05-28 21:53:32 -0500

    Mod 2

--- ts/util/useChangeFormat.ts
+++ ts/util/useChangeFormat.ts
@@ -14,11 +14,24 @@ export function formatTimeUnix(
   locale: Array<string>
 ): string {
   const date = new Date(timestamp * 1000);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
 
-  const formattedTime = date.toLocaleTimeString(locale, {
-    hour: 'numeric',
-    minute: 'numeric',
-  });
+  // Convertir la hora a formato de 24 horas
+  const formattedHours = hours.toString().padStart(2, '0');
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+
+  // Formatear la hora en base al idioma
+  let formattedTime;
+  if (
+    Array.isArray(locale) &&
+    locale.length > 0 &&
+    locale[0].startsWith('en')
+  ) {
+    formattedTime = `${formattedHours}:${formattedMinutes}`;
+  } else {
+    formattedTime = `${formattedHours}h${formattedMinutes}`;
+  }
 
   return formattedTime;
 }
